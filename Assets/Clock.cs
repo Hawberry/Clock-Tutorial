81c3303c2b63ae75dad0eecfa98401efb9ee5c92
B_Molina <bm140314@ohio.edu>
2017-02-07 12:04:55 -0500

    First clock thing

---
+++ Assets/Clock.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Clock : MonoBehaviour {
+{
+	// Update is called once per frame
+	void Update ()
+	{
+			Quaternian euler = Quaternion.Euler(
+				new Vector3(0f, 0f, Time.time * 360f ) ):
+
+				Transform.localRotation = euler;
+	}
+}
